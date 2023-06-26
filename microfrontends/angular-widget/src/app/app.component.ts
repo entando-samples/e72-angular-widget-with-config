e01c67d493d553d8e79ad217e1b4077731359a01
Nathan Shaw <n.shaw@entando.com>
2023-06-26 13:27:36 -0400

    Add mfe-config widget from the react config tutorial and configure the angular-widget to use the input param

--- microfrontends/angular-widget/src/app/app.component.ts
+++ microfrontends/angular-widget/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  @Input() config !: string;
+  name: any;
   title = 'angular-widget';
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['config']) {
+      this.name = JSON.parse(this.config).params.name;
+      console.log("config changes ", this.config, ", params ", this.name)
+    }
+  }
+  
 }
